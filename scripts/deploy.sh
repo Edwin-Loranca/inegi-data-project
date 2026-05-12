94214cce133851bcb8fde97d7710a9a001c74959
Edwin Loranca <edwin_loranca@hotmail.com>
2026-05-12 17:20:28 -0600

    docker-compose.yml file fixed.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,11 +3,9 @@
 cd /home/ec2-user/inegi-data-project
 
 echo "Pulling latest code..."
-
-git pull origin main
+git pull origin master
 
 echo "Rebuilding containers..."
-
 cd ./infra/docker
 
 docker-compose up -d --build
